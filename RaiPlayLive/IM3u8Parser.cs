a2e83fb94911de05d99b5eb03ff47a9173050589
Matteo Contrini <m.contrini@gmail.com>
2018-08-11 16:23:03 +0200

    first version

---
+++ RaiPlayLive/IM3u8Parser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace M3u8Parser
+{
+    public interface IM3u8Parser
+    {
+        List<M3u8Media> Parse();
+
+        void Load(string text);
+    }
+
+    public class M3u8Media
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public long Bandwidth { get; set; }
+        public Resolution Resolution { get; set; }
+        public string Codecs { get; set; }
+        public string Video { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Resolution : IComparable
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Resolution other = (Resolution)obj;
+
+            if (this.Height > other.Height)
+            {
+                return 1;
+            }
+            else if (this.Height < other.Height)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Width}x{Height}";
+        }
+    }
+}
--- RaiPlayLive/Program.cs
+++ RaiPlayLive/Program.cs
@@ -1,12 +1,200 @@
-﻿using System;
+﻿using AngleSharp;
+using AngleSharp.Network.Default;
+using Flurl.Http;
+using M3u8Parser;
+using MediaInfo.DotNetWrapper.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace RaiPlayLive
 {
     class Program
     {
-        static void Main(string[] args)
+        private static IConfiguration configuration;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string dirette = "https://www.raiplay.it/dirette/";
+
+            var requester = new HttpRequester();
+            requester.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36 OPR/54.0.2952.64";
+            configuration = Configuration.Default.WithDefaultLoader(requesters: new[] { requester });
+
+            var document = await BrowsingContext.New(configuration).OpenAsync(dirette);
+
+            var htmlLinks = document.QuerySelectorAll(".channel_rai a");
+            var links = htmlLinks;
+            //var links = htmlLinks.Where(x => x.Attributes["href"].Value == "raipremium");
+
+            foreach (var link in links)
+            {
+                string channel = link.Attributes["href"].Value;
+
+                Console.WriteLine(channel);
+
+                string relinkerUrl = await GetRelinkerUrl(channel);
+                string manifestUrl = await GetManifestUrl(relinkerUrl);
+                string manifestContent = await GetManifest(manifestUrl);
+                var streams = ParseManifest(manifestContent);
+
+                // https://stackoverflow.com/a/1616425/1633924
+                string hlsDirectory = new Uri(new Uri(manifestUrl), ".").OriginalString;
+
+                await OutputStreamsInfo(channel, hlsDirectory, streams);
+                
+                Console.WriteLine();
+                Console.WriteLine("----");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("END");
+            Console.ReadLine();
+        }
+
+        private static async Task<string> DownloadFirstChunk(string channel, string chunklistUrl)
+        {
+            string hlsChunkList = await chunklistUrl.GetStringAsync();
+            var lines = hlsChunkList.Split("\n");
+
+            string chunkFileName = null;
+
+            foreach (var line in lines)
+            {
+                // encrypted
+                if (line.StartsWith("#EXT-X-KEY"))
+                {
+                    return null;
+                }
+
+                // take the .ts file name
+                if (line.Contains(".ts"))
+                {
+                    chunkFileName = line;
+                    break;
+                }
+            }
+
+            if (chunkFileName == null)
+            {
+                return null;
+            }
+
+            string chunkUrl;
+
+            if (chunkFileName.StartsWith("http"))
+            {
+                chunkUrl = chunkFileName;
+            }
+            else
+            {
+                string hlsDirectory = new Uri(new Uri(chunklistUrl), ".").OriginalString;
+                chunkUrl = Path.Combine(hlsDirectory, chunkFileName);
+            }
+
+            string currentDir = Directory.GetCurrentDirectory();
+            await chunkUrl.DownloadFileAsync(currentDir, $"{channel}.ts");
+
+            return Path.Combine(currentDir, $"{channel}.ts");
+        }
+
+        private static async Task OutputStreamsInfo(string channel, string hlsDirectory, List<M3u8Media> streams)
+        {
+            foreach (var stream in streams)
+            {
+                double bandwidth = stream.Bandwidth / 1024.0 / 1024.0;
+                Console.Write($"{stream.Resolution} | {bandwidth:0.00} Mbps | {stream.Codecs}");
+
+                string chunklistUrl;
+                if (stream.Url.StartsWith("http"))
+                {
+                    chunklistUrl = stream.Url;
+                }
+                else
+                {
+                    chunklistUrl = Path.Combine(hlsDirectory, stream.Url);
+                }
+
+                string localSegmentFileName = await DownloadFirstChunk(channel, chunklistUrl);
+
+                if (localSegmentFileName != null)
+                {
+                    using (var info = new MediaInfo.DotNetWrapper.MediaInfo())
+                    {
+                        info.Open(localSegmentFileName);
+                        //Console.WriteLine(info.Option("Info_Parameters"));
+
+                        // includes level. ex: Main@L3.1
+                        string profile = info.Get(StreamKind.Video, 0, "Format_Profile");
+                        string refFrames = info.Get(StreamKind.Video, 0, "Format_Settings_RefFrames");
+                        string gop = info.Get(StreamKind.Video, 0, "Format_Settings_GOP");
+
+                        if (string.IsNullOrEmpty(gop))
+                        {
+                            gop = "?";
+                        }
+
+                        double duration = double.Parse(info.Get(StreamKind.Video, 0, "Duration")) / 1000.0;
+                        string bitrate = info.Get(StreamKind.Video, 0, "BitRate/String").Replace(" ", "");
+
+                        if (string.IsNullOrEmpty(bitrate))
+                        {
+                            bitrate = info.Get(StreamKind.Video, 0, "BitRate_Nominal/String").Replace(" ", "");
+                        }
+
+                        string overall = info.Get(StreamKind.General, 0, "OverallBitRate/String").Replace(" ", "");
+
+                        string mode = info.Get(StreamKind.Video, 0, "BitRate_Mode");
+                        string fps = info.Get(StreamKind.Video, 0, "FrameRate");
+
+                        Console.WriteLine($" | {profile} | ref={refFrames} | GOP {gop} | segment={duration:0.0} | nominal={bitrate:0.###} {mode} | total={overall} | fps={fps}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private static List<M3u8Media> ParseManifest(string manifestContent)
+        {
+            IM3u8Parser parser = new M3u8Parser.M3u8Parser();
+
+            parser.Load(manifestContent);
+            var streams = parser.Parse();
+
+            return streams;
+        }
+
+        private static Task<string> GetManifest(string manifestUrl)
+        {
+            return manifestUrl.GetStringAsync();
+        }
+
+        private static async Task<string> GetManifestUrl(string relinkerUrl)
+        {
+            string fullUrl = relinkerUrl + "&output=45&pl=mon,native";
+            string xmlContent = await fullUrl.GetStringAsync();
+
+            var match = Regex.Match(xmlContent, "<url type=\"content\">(.+?)</url>");
+
+            string masterPlaylist = match.Groups[1].Value;
+
+            return masterPlaylist;
+        }
+
+        static async Task<string> GetRelinkerUrl(string channel)
+        {
+            string channelUrl = $"https://www.raiplay.it/dirette/{channel}";
+
+            var doc = await BrowsingContext.New(configuration).OpenAsync(channelUrl);
+
+            var relinkerUrl = doc.QuerySelector(".Player").Attributes["data-video-url"].Value;
+
+            return relinkerUrl;
         }
     }
 }
